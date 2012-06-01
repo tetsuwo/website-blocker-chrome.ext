6d76c39ecbdb89a3e7d737d75634ad393a6f15d0
unknown <tetsuwo@owner-7bb2c8fba.(none)>
2012-06-02 01:10:49 +0900

    updated test code, modified special thanks

--- src/js/website-blocker.js
+++ src/js/website-blocker.js
@@ -8,128 +8,205 @@
  * Date: 2010-11-21
  */
 
+
 function WebsiteBlocker() {
     this.blockedList = null;
     this.date = null;
     this.time = null;
+
+    // debug mode
     this.debug = false;
+
+    // on/off for time limit function
     this.disabledTimeLimit = false;
 }
 
-WebsiteBlocker.prototype = {
-    blocked: function(id, url) {
-        chrome.tabs.update(id, {
-            url: chrome.extension.getURL('blocked.html') + '?url=' + encodeURIComponent(url)
-        });
-    },
-    isBlocked: function(taburl, regexp, time) {
-        if (taburl.search(new RegExp(regexp, 'ig')) > 5) {
-            if (this.disabledTimeLimit) {
+
+(function( WB, undef ) {
+
+
+/**
+ * For Block the tab of target ID
+ *
+ * @param id  {String} ID of target tab
+ * @param url {String} URL of target tab
+ */
+WB.prototype.blocked = function(id, url) {
+    chrome.tabs.update(id, {
+        url: chrome.extension.getURL('blocked.html') + '?url=' + encodeURIComponent(url)
+    });
+};
+
+
+/**
+ * Is blocked the URL?
+ *
+ * @param url    {String}
+ * @param regexp {String}
+ * @param time   {Array}
+ */
+WB.prototype.isBlocked = function(url, regexp, time) {
+
+    this.logger(url.search(new RegExp(regexp, 'ig')));
+
+    if (url.search(new RegExp(regexp, 'ig')) > 5) {
+        if (this.disabledTimeLimit) {
+            return true;
+        }
+
+        if (time.length === 0) {
+            return true;
+        }
+
+        for (var i in time) {
+            var c = time[i].split('-');
+
+            if (Number(c[0]) <= Number(this.time) && Number(this.time) <= Number(c[1])) {
                 return true;
             }
+        }
+    }
 
-            if (time.length === 0) {
-                return true;
+    return false;
+};
+
+
+/**
+ * To string
+ *
+ * @param list {Array}
+ */
+WB.prototype.toString = function(list) {
+
+    var result = [];
+
+    for (var key in list) {
+        var row = list[key];
+        result.push(row.url + ' ' + row.time.join(','));
+    }
+
+    return result.join('\n');
+};
+
+
+/**
+ * To format
+ *
+ * @param text {String}
+ */
+WB.prototype.toFormat = function(text) {
+
+    var rows, row, URLTime, line, BLOCKED = [];
+    rows = text.split('\n');
+
+    if (rows.length > 0) {
+        for (var i = 0; i < rows.length; i++) {
+            row = rows[i].trim();
+            if (!row) continue;
+            URLTime = row.split(' ');
+            line = { url: URLTime[0], time: [], regexp: null };
+
+            if (URLTime.length === 2) {
+                line.time = URLTime[1].split(',');
+            }
+            else if (URLTime.length > 2) {
+                line.time = row.replace(/\s/g, '').replace(URLTime[0], '').split(',');
             }
 
-            for (var i in time) {
-                var c = time[i].split('-');
+            line.regexp = URLTime[0].replace(/\./g, '\\.');
+            BLOCKED.push(line);
+        }
+    }
 
-                if (Number(c[0]) <= Number(this.time) && Number(this.time) <= Number(c[1])) {
+    return BLOCKED;
+};
+
+
+/**
+ * Make time
+ *
+ * @param none
+ */
+WB.prototype.makeTime = function() {
+
+    this.date = new Date();
+    var hh = this.date.getHours();
+    var mm = this.date.getMinutes();
+
+    if (hh < 10) {
+        hh = '0' + hh;
+    }
+
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+
+    return this.time = hh.toString() + mm.toString();
+};
+
+
+/**
+ * Check URL
+ *
+ * @param id       {String}
+ * @param url      {String}
+ * @param testmode {Boolean}
+ */
+WB.prototype.checkUrl = function(id, url, testmode) {
+
+    this.blockedList = ls.get('blocked_list');
+
+    if (this.blockedList) {
+        this.makeTime();
+        this.disabledTimeLimit = ls.get('time_limit_disabled');
+
+        for (var key in this.blockedList) {
+            this.logger(this.blockedList[key].regexp);
+            if (this.isBlocked(url, this.blockedList[key].regexp, this.blockedList[key].time)) {
+                if (testmode) {
                     return true;
                 }
+                this.blocked(id, url);
+                break;
             }
         }
+    }
 
-        return false;
-    },
-    toString: function(list) {
-        var result = [];
+    return false;
+};
 
-        for (var key in list) {
-            var row = list[key];
-            result.push(row.url + ' ' + row.time.join(','));
-        }
 
-        return result.join('\n');
-    },
-    toFormat: function(text) {
-        var rows, row, URLTime, line, BLOCKED = [];
-        rows = text.split('\n');
-
-        if (rows.length > 0) {
-            for (var i = 0; i < rows.length; i++) {
-                row = rows[i].trim();
-                if (!row) continue;
-                URLTime = row.split(' ');
-                line = { url: URLTime[0], time: [], regexp: null };
-
-                if (URLTime.length === 2) {
-                    line.time = URLTime[1].split(',');
-                }
-                else if (URLTime.length > 2) {
-                    line.time = row.replace(/\s/g, '').replace(URLTime[0], '').split(',');
-                }
-
-                line.regexp = URLTime[0].replace('.', '\\.');
-                BLOCKED.push(line);
-            }
-        }
-
-        return BLOCKED;
-    },
-    makeTime: function() {
-        this.date = new Date();
-        var hh = this.date.getHours();
-        var mm = this.date.getMinutes();
-
-        if (hh < 10) {
-            hh = '0' + hh;
-        }
-
-        if (mm < 10) {
-            mm = '0' + mm;
-        }
-
-        return this.time = hh.toString() + mm.toString();
-    },
-    checkUrl: function(id, url, testmode) {
-        this.blockedList = ls.get('blocked_list');
-
-        if (this.blockedList) {
-            this.makeTime();
-            this.disabledTimeLimit = ls.get('time_limit_disabled');
-
-            for (var key in this.blockedList) {
-                this.logger(this.blockedList[key].regexp);
-                if (this.isBlocked(url, this.blockedList[key].regexp, this.blockedList[key].time)) {
-                    if (testmode) {
-                        return true;
-                    }
-                    this.blocked(id, url);
-                    break;
-                }
-            }
-        }
-
-        return false;
-    },
-    logger: function(a) {
-        if (this.debug) {
-            console.log(a);
-        }
-    },
-    run: function(tab) {
-        this.logger(tab);
-
-        if (ls.get('blocked_disabled')) {
-            return false;
-        }
-
-        if (tab.url.search(/https?:/) === 0) {
-            this.checkUrl(tab.id, tab.url, false);
-        }
-
-        return true;
+/**
+ * Logger
+ *
+ * @param a {Any}
+ */
+WB.prototype.logger = function(a) {
+    if (this.debug) {
+        console.log(a);
     }
 };
+
+
+/**
+ * Run Website Blocker!
+ *
+ * @param tab {Object}
+ */
+WB.prototype.run = function(tab) {
+
+    this.logger(tab);
+
+    if (ls.get('blocked_disabled')) {
+        return false;
+    }
+
+    if (tab.url.search(/https?:/) === 0) {
+        this.checkUrl(tab.id, tab.url, false);
+    }
+
+    return true;
+};
+
+
+})(WebsiteBlocker);
--- test/test.js
+++ test/test.js
@@ -2,14 +2,63 @@ describe('WebsiteBlocker', function() {
 
     var testdata = {
     };
+    var blockedArray = [];
 
     it('default', function() {
         var wb = new WebsiteBlocker();
+
         expect(wb.blockedList).toBeNull();
         expect(wb.date).toBeNull();
         expect(wb.time).toBeNull();
         expect(wb.debug).toBeFalsy();
         expect(wb.disabledTimeLimit).toBeFalsy();
+    });
+
+    it('isBlocked', function() {
+        var wb = new WebsiteBlocker();
+        wb.debug = true;
+        wb.disabledTimeLimit = true;
+
+        expect(wb.debug).toBeTruthy(
+            wb.isBlocked('http://www.yahoo.co.jp', 'yahoo\.co\.jp', [])
+        );
+    });
+
+    it('toFormat', function() {
+        var wb = new WebsiteBlocker();
+        wb.debug = true;
+        wb.disabledTimeLimit = true;
+
+        var text = '';
+        text += '(facebook|twitter).com 0000-0730,0945-1159\n';
+        text += 'yahoo.co.jp';
+        var data = blockedArray = wb.toFormat(text);
+        console.log(data);
+
+        expect(data.length).toEqual(2);
+
+        expect(data[0].url).toEqual('(facebook|twitter).com');
+        expect(data[0].regexp).toEqual('(facebook|twitter)\\.com');
+        expect(data[0].time.length).toEqual(2);
+
+        expect(data[1].url).toEqual('yahoo.co.jp');
+        expect(data[1].regexp).toEqual('yahoo\\.co\\.jp');
+        expect(data[1].time.length).toEqual(0);
+    });
+
+    it('toString', function() {
+        var wb = new WebsiteBlocker();
+        wb.debug = true;
+        wb.disabledTimeLimit = true;
+
+        var text = '';
+        text += '(facebook|twitter).com 0000-0730,0945-1159\n';
+        text += 'yahoo.co.jp ';
+
+        var data = wb.toString(blockedArray);
+        console.log(data);
+
+        expect(data).toEqual(text);
     });
 
 });
