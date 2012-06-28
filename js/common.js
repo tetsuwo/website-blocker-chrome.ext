4604d0093286f97c2d90b2e5254f9dfcd8a45313
tetsuwo <tetsuwo.oishi@gmail.com>
2012-06-28 15:56:55 +0900

    import js files

---
+++ js/common.js
@@ -0,0 +1,18 @@
+/**
+ * Website Blocker
+ * Copyright (C) Tetsuwo Oishi All Rights Reserved.
+ *
+ * @author Tetsuwo OISHI <tetsuwo.oishi@gmail.com>
+ * @createdAt 2010/11/18
+ * @updatedAt 2011/02/22
+ */
+
+// Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-17392775-5']);
+_gaq.push(['_trackPageview']);
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
