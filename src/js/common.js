347622145cb938858947183d1b42fc63b8a3d7f4
tetsuwo <tetsuwo.oishi@gmail.com>
2012-07-06 18:20:13 +0900

    added welcome.html, modified background.html

--- src/js/common.js
+++ src/js/common.js
@@ -1,18 +1,31 @@
 /**
- * Website Blocker
- * Copyright (C) Tetsuwo Oishi All Rights Reserved.
+ * common.js for chrome extension
  *
  * @author Tetsuwo OISHI <tetsuwo.oishi@gmail.com>
- * @createdAt 2010/11/18
- * @updatedAt 2011/02/22
  */
+
+// Set Environment Vars
+MIGRATION_VERSION = '1';
+STORAGE_NAMESPACE = '__wb_' + MIGRATION_VERSION;
 
 // Google Analytics
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-17392775-5']);
 _gaq.push(['_trackPageview']);
-(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+(function(d) {
+    var ga = d.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
     ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+    var s = d.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})(document);
+
+// Tracking
+function _trace(category, action, label) {
+    try {
+        _gaq.push(['_trackEvent', category, action, label]);
+    } catch (e) {;}
+}
+
+_trace('SETTING', 'MIGRATION_VERSION', MIGRATION_VERSION);
