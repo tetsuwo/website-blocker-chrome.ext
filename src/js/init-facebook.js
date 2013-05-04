c5f2560eb1972ded3a343c96376c831e47ac9b10
tetsuwo <tetsuwo.oishi@gmail.com>
2013-05-04 17:42:12 +0900

    added Welcome page

---
+++ src/js/init-facebook.js
@@ -0,0 +1,9 @@
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "http://connect.facebook.net/ja_JP/all.js#xfbml=1&appId=219688404715111";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
