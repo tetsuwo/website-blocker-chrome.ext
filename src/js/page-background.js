a61519801b8cf34fe381d9e8f0f8b44c46db4e3a
tetsuwo <tetsuwo.oishi@gmail.com>
2013-05-06 18:49:40 +0900

    clean up code

--- src/js/page-background.js
+++ src/js/page-background.js
@@ -3,32 +3,16 @@ var afterFunction = null;
 var WB = new WebsiteBlocker();
 
 chrome.tabs.onCreated.addListener(function(tab) {
-//    console.log('tabs.onCreated:');
-//    console.log(tab);
     WB.run(tab);
 });
 
 chrome.tabs.onActivated.addListener(function(info) {
-//    console.log('tabs.onActivated:');
-//    console.log(info);
     chrome.tabs.get(info.tabId, function(tab) {
         WB.run(tab);
     });
 });
 
-//chrome.tabs.onAttached.addListener(function(tabId, info) {
-//    console.log('tabs.onAttached:');
-//    console.log(info);
-//});
-
-//chrome.tabs.onHighlighted.addListener(function(info) {
-//    console.log('tabs.onHighlighted:');
-//    console.log(info);
-//});
-
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-//    console.log('tabs.onUpdated:');
-//    console.log(tab);
     if (changeInfo.status === 'loading') {
         WB.run(tab);
         return;
@@ -55,13 +39,11 @@ function checkCurrentTab() {
 };
 
 chrome.extension.onRequest.addListener(function(tab) {
-//    console.log('extension.onRequest:');
-//    console.log(tab);
     afterFunction(tab);
 });
 
 if (!db.get('regular_visitor', false)) {
-    chrome.tabs.create({ url: 'welcome.html', selected: true })
-    db.set('regular_visitor', true);
+    //chrome.tabs.create({ url: 'welcome.html', selected: true })
+    //db.set('regular_visitor', true);
 }
 
